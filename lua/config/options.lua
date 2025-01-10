b4a0f578fc5ae15ea419ef20c1b041da61778520
phenriquefernandes <28553108+phenriquefernandes@users.noreply.github.com>
2025-01-10 19:11:00 -0300

    update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,30 @@
+--
 vim.o.number = true
 vim.o.relativenumber = true
 
--- buffer options
+--
+vim.o.wrap = false
+
+--
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undodir = os.getenv 'HOME' .. '/.vim/undodir'
+vim.o.undofile = true
+
+--
+vim.o.termguicolors = true
+
+--
+vim.o.scrolloff = 8
+
+--
+vim.o.signcolumn = 'yes'
+
+--
+vim.o.updatetime = 50
+
+--
+vim.o.colorcolumn = '120'
+
+--
 vim.bo.smartindent = true
