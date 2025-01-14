76970c97e0640be8fdd955c1fcbca0beb0a9ef7a
phenriquefernandes <28553108+phenriquefernandes@users.noreply.github.com>
2025-01-14 20:21:10 -0300

    add 'mfussenegger/nvim-jdtls' plugin

---
+++ after/plugin/java.lua
@@ -0,0 +1,8 @@
+local home = os.getenv 'HOME' or os.getenv 'USERPROFILE'
+
+local config = {
+  cmd = { home .. '/.local/share/nvim/mason/bin/jdtls' },
+  root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+}
+
+require('jdtls').start_or_attach(config)
