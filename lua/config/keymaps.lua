2df7571a32e46ce6c3694161dbd2f57b2bd45f0c
phenriquefernandes <28553108+phenriquefernandes@users.noreply.github.com>
2025-01-16 13:40:56 -0300

    update keybindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,11 +7,14 @@ vim.keymap.set({ 'n', 'i', 'v' }, '<C-q>', '<CMD>quit<CR>')
 vim.keymap.set('n', '<C-a>', 'gg0vGg_')
 
 --
--- vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
 vim.keymap.set({ 'n', 'i', 'v' }, '<C-q>', '<CMD>quit<CR>')
+
+--
 vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
 vim.keymap.set({ 'n' }, '<leader>Y', [["+Y]])
-vim.keymap.set('x', '<leader>p', [["_dP]])
+
+--
+vim.keymap.set('n', 'x', '"_x')
 
 --
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
@@ -36,10 +39,36 @@ vim.keymap.set('n', 'H', '_')
 vim.keymap.set('n', 'L', 'g_')
 
 --
-vim.keymap.set('n', '<C-b>', '<NOP>')
-
---
 vim.keymap.set('n', '<Esc>', '<CMD>nohlsearch<CR>')
 
 --
 vim.keymap.set('n', '<leader>S', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+--
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+vim.keymap.set('n', '<C-right>', '<C-w><')
+vim.keymap.set('n', '<C-left>', '<C-w>>')
+vim.keymap.set('n', '<C-up>', '<C-w>+')
+vim.keymap.set('n', '<C-down>', '<C-w>-')
+
+--
+vim.keymap.set('n', 'te', ':tabedit')
+vim.keymap.set('n', '<tab>', ':tabnext<Return>')
+vim.keymap.set('n', '<s-tab>', ':tabprev<Return>')
+
+--
+vim.keymap.set('n', 'ss', ':split<Return>')
+vim.keymap.set('n', 'sv', ':vsplit<Return>')
+
+--
+vim.keymap.set('n', 'sh', '<C-w>h')
+vim.keymap.set('n', 'sk', '<C-w>k')
+vim.keymap.set('n', 'sj', '<C-w>j')
+vim.keymap.set('n', 'sl', '<C-w>l')
+
+--
+vim.keymap.set('n', '+', '<C-a>')
+vim.keymap.set('n', '-', '<C-x>')
