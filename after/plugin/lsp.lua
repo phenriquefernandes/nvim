1537d9ffb339e699d5a360b27359216678d7fa38
phenriquefernandes <28553108+phenriquefernandes@users.noreply.github.com>
2025-01-16 13:41:24 -0300

    add 'gopls' to lsp servers

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -61,6 +61,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
 local servers = {
+  gopls = {},
   rust_analyzer = {},
   lua_ls = {
     settings = {
