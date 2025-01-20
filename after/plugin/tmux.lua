374673c8bd7afb661860a74611113f0d1309d142
phenriquefernandes <28553108+phenriquefernandes@users.noreply.github.com>
2025-01-20 19:02:55 -0300

    add 'christoomey/vim-tmux-navigator' plugin

---
+++ after/plugin/tmux.lua
@@ -0,0 +1,5 @@
+vim.keymap.set('n', '<c-h>', '<cmd><C-U>TmuxNavigateLeft<CR>')
+vim.keymap.set('n', '<c-j>', '<cmd><C-U>TmuxNavigateDown<CR>')
+vim.keymap.set('n', '<c-k>', '<cmd><C-U>TmuxNavigateUp<CR>')
+vim.keymap.set('n', '<c-l>', '<cmd><C-U>TmuxNavigateRight<CR>')
+vim.keymap.set('n', '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<CR>')
