d1a866adeafbcf4937eeb68024a7d00219eccee7
phenriquefernandes <28553108+phenriquefernandes@users.noreply.github.com>
2025-01-28 14:23:16 -0300

    update cmdline.lua

--- after/plugin/cmdline.lua
+++ after/plugin/cmdline.lua
@@ -1,2 +1,1 @@
 vim.keymap.set('n', 'Q', ':Telescope cmdline<CR>', { noremap = true, desc = 'Cmdline' })
-vim.keymap.set('n', '<leader><leader>', ':Telescope cmdline<CR>', { noremap = true, desc = 'Cmdline' })
